cf5e983fad4872d4b894037652eb225b85aa07c2
Joshua Luongo <josh@jrapps.com.au>
2014-06-04 16:09:36 +1000

    Added project files

---
+++ jrUUID/jrUUID/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  jrUUID
+//
+//  Created by Joshua Luongo on 16/04/2014.
+//  Copyright (c) 2014 JR Apps. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[])
+{
+    return NSApplicationMain(argc, argv);
+}
